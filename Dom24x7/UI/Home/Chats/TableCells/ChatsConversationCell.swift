346a096088e6be649cacaff79be5c67ec77e4d26
alimtolman <alimtolman@mail.ru>
2021-07-06 16:28:00 +0300

    commit

---
+++ Dom24x7/UI/Home/Chats/TableCells/ChatsConversationCell.swift
@@ -0,0 +1,16 @@
+//
+//  ChatsConversationCell.swift
+//  Dom24x7
+//
+//  Created by Alim Gershishev on 06.07.2021.
+//
+
+import UIKit
+
+class ChatsConversationCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bodyLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}

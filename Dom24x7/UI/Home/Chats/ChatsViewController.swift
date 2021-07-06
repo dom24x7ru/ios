70d87daef54e60e8db666c030993652cbea655b5
alimtolman <alimtolman@mail.ru>
2021-07-06 16:28:36 +0300

    commit

--- Dom24x7/UI/Home/Chats/ChatsViewController.swift
+++ Dom24x7/UI/Home/Chats/ChatsViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import MaterialComponents
 
-class ChatsViewController: UIViewController, MDCAppBarNavigationControllerDelegate {
+class ChatsViewController: UITableViewController, MDCAppBarNavigationControllerDelegate {
     
+    private let loadingBar = LoadingBar()
     public var appBarController: MDCAppBarNavigationController!
     
     
@@ -17,6 +18,21 @@ class ChatsViewController: UIViewController, MDCAppBarNavigationControllerDelega
         super.viewDidLoad()
         
         appBarController.delegate = self
+        tableView.separatorInset = .zero
+        tableView.tableFooterView = UIView()
+        
+        loadingBar.add(to: view, style: .large, color: .gray, offset: viewOffset)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        ClientData.imChannels.onUpdate = {
+            self.loadingBar.remove()
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        ClientData.imChannels.onUpdate = nil
     }
     
     // MARK: AppBarNavigationController
@@ -26,6 +42,31 @@ class ChatsViewController: UIViewController, MDCAppBarNavigationControllerDelega
         appBarViewController.navigationBar.inkColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.1)
         appBarViewController.navigationBar.tintColor = .white
         appBarViewController.navigationBar.titleTextColor = .white
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ClientData.imChannels.data.count
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatsConversationCell", for: indexPath) as! ChatsConversationCell
+        let chat = ClientData.imChannels.data[indexPath.row]
+        
+        cell.titleLabel.text = chat.title ?? Localization.noTitle
+        cell.bodyLabel.text = chat.lastMessage?.body.text ?? Localization.noMessagesHereYet
+        cell.dateLabel.text = DateHelper.formatForIM(timestamp: chat.lastMessage?.createdAt ?? 0)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }

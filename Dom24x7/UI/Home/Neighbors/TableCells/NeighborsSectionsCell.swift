cbb59b74aaaa1e0f8fafeb67c7e4c64ecf69522a
alimtolman <alimtolman@mail.ru>
2021-07-08 14:40:29 +0300

    add

---
+++ Dom24x7/UI/Home/Neighbors/TableCells/NeighborsSectionsCell.swift
@@ -0,0 +1,18 @@
+//
+//  NeighborsSectionsCell.swift
+//  Dom24x7
+//
+//  Created by Alim Gershishev on 07.07.2021.
+//
+
+import UIKit
+
+class NeighborsSectionsCell: UITableViewCell {
+
+    @IBOutlet var sectionLabel: UILabel!
+    @IBOutlet var floorsLabel: UILabel!
+    @IBOutlet var flatsLabel: UILabel!
+    @IBOutlet var filledLabel: UILabel!
+    @IBOutlet var personsLabel: UILabel!
+    
+}

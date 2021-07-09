59fe8fbc721cd6a64b6bc530134c6df9c784ca71
alimtolman <alimtolman@mail.ru>
2021-07-09 12:14:20 +0300

    commit

--- Dom24x7/UI/Home/Neighbors/NeighborsViewController.swift
+++ Dom24x7/UI/Home/Neighbors/NeighborsViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 import MaterialComponents
 
-class NeighborsViewController: UIViewController, MDCAppBarNavigationControllerDelegate {
+class NeighborsViewController: UITableViewController, MDCAppBarNavigationControllerDelegate {
     
+    private let loadingBar = LoadingBar()
+    private var indexes: [Int] = []
     public var appBarController: MDCAppBarNavigationController!
     
     
@@ -17,6 +19,32 @@ class NeighborsViewController: UIViewController, MDCAppBarNavigationControllerDe
         super.viewDidLoad()
         
         appBarController.delegate = self
+        tableView.separatorInset = .zero
+        tableView.tableFooterView = UIView()
+        
+        loadingBar.add(to: view, style: .large, color: .gray, offset: viewOffset)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        ClientData.sections.onUpdate = {
+            self.indexes = ClientData.sections.data.keys.sorted()
+            
+            self.loadingBar.remove()
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        ClientData.sections.onUpdate = nil
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! NeighborsFloorsViewController
+        let selectedCell = sender as! NeighborsSectionCell
+        let floors = ClientData.sections.data[selectedCell.index]!.floors
+        
+        destination.floors = floors
+        destination.indexes = floors.keys.sorted()
     }
     
     // MARK: AppBarNavigationController
@@ -26,6 +54,35 @@ class NeighborsViewController: UIViewController, MDCAppBarNavigationControllerDe
         appBarViewController.navigationBar.inkColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.1)
         appBarViewController.navigationBar.tintColor = .white
         appBarViewController.navigationBar.titleTextColor = .white
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return indexes.count
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 158
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NeighborsSectionCell", for: indexPath) as! NeighborsSectionCell
+        let sectionNumber = indexes[indexPath.row]
+        let section = ClientData.sections.data[sectionNumber]!
+        
+        cell.index = sectionNumber
+        cell.sectionLabel.text = Localization.section + " " + String(sectionNumber)
+        cell.floorsLabel.text = Localization.floors + ": " + String(section.floorsCount)
+        cell.flatsLabel.text = Localization.flats + ": " + String(section.flatMin) + " - " + String(section.flatMax)
+        cell.populatedLabel.text = Localization.populated + ": " + String(section.populated) + " " + Localization.of + " " + String(section.flatsCount)
+        cell.personsLabel.text = Localization.persons + ": " + String(section.persons)
+                
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }

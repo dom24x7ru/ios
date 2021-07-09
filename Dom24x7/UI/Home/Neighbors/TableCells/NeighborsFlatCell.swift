ba7011332bce9f28c05233f3d5e4a62697a7885b
alimtolman <alimtolman@mail.ru>
2021-07-09 14:39:24 +0300

    commit

---
+++ Dom24x7/UI/Home/Neighbors/TableCells/NeighborsFlatCell.swift
@@ -0,0 +1,19 @@
+//
+//  NeighborsFlatCell.swift
+//  Dom24x7
+//
+//  Created by Alim Gershishev on 09.07.2021.
+//
+
+import UIKit
+
+class NeighborsFlatCell: UITableViewCell {
+    
+    public var index: Int = 0
+    
+    @IBOutlet var flatLabel: UILabel!
+    @IBOutlet var personsLabel: UILabel!
+    @IBOutlet var roomsLabel: UILabel!
+    @IBOutlet var sizeLabel: UILabel!
+    
+}
